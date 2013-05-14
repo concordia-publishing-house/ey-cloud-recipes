e41f76a8cfcfc80b56996ac36b256d486fe553d1
Bob Lail <bob.lail@cph.org>
2013-05-14 12:44:53 -0500

    Rewrote mdbtools recipe

--- cookbooks/mdbtools/recipes/default.rb
+++ cookbooks/mdbtools/recipes/default.rb
@@ -3,18 +3,16 @@
 # Recipe:: default
 #
 
-git "#{Chef::Config[:file_cache_path]}/mdbtools" do
-   repository "git://github.com/brianb/mdbtools.git"
-   revision "eeadcb54f3e0df8c513ff598ff56be1c77f63acf"
-   action :sync
-   notifies :run, "bash[install_mdbtools]"
-end
+TEMP_PATH = "/etc/chef-custom/tmp"
 
 bash "install_mdbtools" do
-   cwd "#{Chef::Config[:file_cache_path]}/mdbtools"
-   code <<-EOH
-     ./autogen.sh
-     make
-     make install
-     EOH
+  code <<-BASH
+    mkdir -p #{TEMP_PATH}
+    git clone git://github.com/brianb/mdbtools.git #{TEMP_PATH}/mdbtools
+    cd "#{TEMP_PATH}/mdbtools"
+    ./autogen.sh
+    make
+    make install
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+  BASH
 end
