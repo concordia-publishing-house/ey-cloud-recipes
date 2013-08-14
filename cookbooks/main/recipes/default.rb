d522be623714ec81346b27e99e469b68bbdbb172
Bob Lail <bob.lail@cph.org>
2013-08-14 10:31:54 -0500

    Unite doesn't need mdbtools

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -5,7 +5,7 @@
 #end
 
 # use mdbtools recipe
-include_recipe "mdbtools"
+# include_recipe "mdbtools"
 
 # uncomment to turn on thinking sphinx 2/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
 # include_recipe "sphinx"
