09c2ccafa0dcc91e6fe04a6ee15fc146ba679c91
Bob Lail <bob.lail@cph.org>
2013-08-28 14:38:33 -0500

    The redis recipe already names 'redis_instance' in /etc/hosts; so we can just use that here

--- cookbooks/redis-yml/recipes/default.rb
+++ cookbooks/redis-yml/recipes/default.rb
@@ -1,7 +1,8 @@
 if ['app_master', 'app', 'solo'].include?(node[:instance_role])
   
-  instances = node[:engineyard][:environment][:instances]
-  redis_instance = instances.length == 1 ? instances[0] : instances.find { |instance| instance['name'] == 'redis' }
+  # instances = node[:engineyard][:environment][:instances]
+  # redis_instance = instances.length == 1 ? instances[0] : instances.find { |instance| instance['name'] == 'redis' }
+  redis_instance = true # <-- if we're including this recipe; we're expecting redis
   
   if redis_instance
     node[:applications].each do |app, data|
@@ -13,7 +14,7 @@ if ['app_master', 'app', 'solo'].include?(node[:instance_role])
         backup 0
         variables({
           :environment => node[:environment][:framework_env],
-          :hostname => redis_instance[:hostname]
+          :hostname => "redis_instance"
         })
       end
     end
