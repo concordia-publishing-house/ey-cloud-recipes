9c946971e9b17239934f39e57b8e525ad064fe8b
Bob Lail <bob.lail@cph.org>
2013-08-28 14:37:49 -0500

    Used a less insane way of finding the VM named 'redis'

--- cookbooks/redis/recipes/default.rb
+++ cookbooks/redis/recipes/default.rb
@@ -71,7 +71,7 @@ end
 
 if ['solo', 'app', 'app_master', 'util'].include?(node[:instance_role])
   
-  redis_instance = instances.length == 1 ? instances[0] : instances.find { |i| i[:name].to_s[/redis/] }
+  redis_instance = instances.length == 1 ? instances[0] : instances.find { |instance| instance['name'] == 'redis' }
   
   if redis_instance
     ip_address = `ping -c 1 #{redis_instance[:private_hostname]} | awk 'NR==1{gsub(/\\(|\\)/,"",$3); print $3}'`.chomp
