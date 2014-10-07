56538359d05b4cc767aa19f37c551d5de7e26ac6
Luke Booth <luke.booth@cph.org>
2014-10-07 10:01:56 -0500

    [skip] Uncommented sidekiq and redis-yml recipes (1m)

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -23,7 +23,7 @@ include_recipe "dotfiles"
 # include_recipe "ban"
 
 # uncomment to use the sidekiq recipe. See cookbooks/sidekiq/readme.md for documentation.
-# include_recipe "sidekiq"
+include_recipe "sidekiq"
 
 #uncomment to turn on memcached
 # include_recipe "memcached"
@@ -63,7 +63,7 @@ include_recipe "dotfiles"
 include_recipe "resque"
 
 #uncomment to run redis.yml recipe
-# include_recipe "redis-yml"
+include_recipe "redis-yml"
 
 #uncomment to run the resque-scheduler recipe
 # include_recipe "resque-scheduler"
