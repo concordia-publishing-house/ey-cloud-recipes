114feecb65e6138093d857e4b61b51ff6341c9e6
Luke Booth <Luke.Booth@cph.org>
2014-10-08 07:30:15 -0500

    [skip] Only used the sidekiq and dotfiles recipes for Unite (1m)

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -60,16 +60,16 @@ include_recipe "sidekiq"
 #include_recipe "mongodb"
 
 #uncomment to run the resque recipe
-include_recipe "resque"
+# include_recipe "resque"
 
 #uncomment to run redis.yml recipe
-include_recipe "redis-yml"
+# include_recipe "redis-yml"
 
 #uncomment to run the resque-scheduler recipe
 # include_recipe "resque-scheduler"
 
 #uncomment to run the redis recipe
-include_recipe "redis"
+# include_recipe "redis"
 
 #uncomment to run the api-keys-yml recipe
 # include_recipe "api-keys-yml"
@@ -109,7 +109,7 @@ include_recipe "redis"
 # include_recipe "jenkins"
 
 #uncomment to include the newrelic_server_monitoring recipe
-require_recipe "newrelic_server_monitoring"
+# require_recipe "newrelic_server_monitoring"
 
 #enable Extension modules for a given Postgresql database
 # if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
