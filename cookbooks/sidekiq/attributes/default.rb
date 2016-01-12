f115b079955d074a3095c283707f6df9681adf31
Chase Clettenberg <chase.clettenberg@cph.org>
2016-01-12 14:10:56 -0600

    [skip] Added sidekiq queue for Attendance Sheets (5m)

--- cookbooks/sidekiq/attributes/default.rb
+++ cookbooks/sidekiq/attributes/default.rb
@@ -9,13 +9,13 @@ default[:sidekiq] = {
   # only be installed on to a utility instance that matches
   # the name
   :utility_name => 'sidekiq',
-  
+
   # Number of workers (not threads)
   :workers => 1,
-  
+
   # Concurrency
   :concurrency => 25,
-  
+
   # Queues
   :queues => {
     # :queue_name => priority
@@ -23,9 +23,10 @@ default[:sidekiq] = {
     "members:email_invites" => 1,
     "members:church_directories" => 1,
     "members:contribution_statements" => 1,
-    "members:mailing_labels" => 1
+    "members:mailing_labels" => 1,
+    "members:attendance_sheets" => 1
   },
-  
+
   # Verbose
   :verbose => false
 }
